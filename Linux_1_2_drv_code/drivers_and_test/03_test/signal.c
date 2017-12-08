563a61efc5ee5c64806b95305b7e46aea9417cd9
oucaijun <oucaijun.happy@163.com>
2017-12-08 20:17:56 +0800

    Update signal.c

--- Linux_1_2_drv_code/drivers_and_test/03_test/signal.c
+++ Linux_1_2_drv_code/drivers_and_test/03_test/signal.c
@@ -1,22 +1,70 @@
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <poll.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+
+/* fifthdrvtest 
+  */
+int fd;
+
+//信号处理函数
 void my_signal_fun(int signum)
 {
-	static int cnt = 0;
-	printf("signal = %d, %d times\n", signum, ++cnt);
+	unsigned char key_val;
+	read(fd, &key_val, 1);
+	printf("key_val: 0x%x\n", key_val);
 }
 
 int main(int argc, char **argv)
 {
+	unsigned char key_val;
+	int ret;
+	int Oflags;
 
-	signal(SIGUSR1, my_signal_fun);
+	//在应用程序中捕捉SIGIO信号（由驱动程序发送）
+	//在驱动中: kill_fasync(&fasync, SIGIO, POLL_IN);
+	signal(SIGIO, my_signal_fun);
 	
+	fd = open("/dev/buttons", O_RDWR);
+	if (fd < 0)
+	{
+		printf("can't open!\n");
+		return 1;
+	}
+
+	//将当前进程PID设置为fd文件所对应驱动程序将要发送SIGIO,SIGUSR信号进程PID
+	fcntl(fd, F_SETOWN, getpid());	
+	//获取fd的打开方式
+	Oflags = fcntl(fd, F_GETFL); 
+	//将fd的打开方式设置为FASYNC --- 即 支持异步通知
+	//该行代码执行会触发 驱动程序中 file_operations->fasync 函数 ------fasync函数调用fasync_helper初始化一个fasync_struct结构体，该结构体描述了将要发送信号的进程PID (fasync_struct->fa_file->f_owner->pid)
+	fcntl(fd, F_SETFL, Oflags | FASYNC);
 	while (1)
 	{
 		sleep(1000);
-	}
+	}	
 	return 0;
 }
 
+/*
+根据下面的文章做了部分改动：
+kill_async与fasync_helper函数
+http://blog.csdn.net/luckywang1103/article/details/42466225
+forth_drv_fasync
+
+linux驱动的异步通知（kill_fasync,fasync）---- 驱动程序向应用程序发送信号
+http://jingpin.jikexueyuan.com/article/56305.html
+        //将当前进程PID设置为fd文件所对应驱动程序将要发送SIGIO,SIGUSR信号进程PID
+	fcntl(fd, F_SETOWN, getpid());	
+	//获取fd的打开方式
+	Oflags = fcntl(fd, F_GETFL); 
+	//将fd的打开方式设置为FASYNC --- 即 支持异步通知
+	//该行代码执行会触发 驱动程序中 file_operations->fasync 函数 ------fasync函数调用fasync_helper初始化一个fasync_struct结构体，该结构体描述了将要发送信号的进程PID (fasync_struct->fa_file->f_owner->pid)
+	fcntl(fd, F_SETFL, Oflags | FASYNC);
+*/
